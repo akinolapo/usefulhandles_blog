7519ecb3651f458cc4a08aa56adc2f6130136b90
Peace Akinola <akinolapo@gmail.com>
2022-07-26 12:09:03 +0100

    change site name

--- .history/components/Header_20220525143555.jsx
+++
@@ -1,9 +0,0 @@
-import React, {user} from 'react'
-
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
-
-export default Header
--- .history/components/Header_20220726115559.jsx
+++ .history/components/Header_20220726115559.jsx
@@ -18,7 +18,7 @@ const Header = () => {
             <div className="border-b w-full inline-block border-blue-400 py-8">
                 <div className="md:float-left block">
                     <Link href='/'>
-                        <span className='cursor-pointer font-bold text-4xl text-white'>UsefulHandles</span>
+                        <span className='cursor-pointer font-bold text-4xl text-white'>WordP</span>
                     </Link>
                 </div>
 
