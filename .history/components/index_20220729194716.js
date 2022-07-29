065d0c1ed208732ce3887be87277ee56a060cf97
Peace Akinola <akinolapo@gmail.com>
2022-07-29 19:53:35 +0100

    AdjacentPost

--- .history/components/index_20220729194716.js
+++ .history/components/index_20220729194716.js
@@ -8,6 +8,10 @@ export {default as CommentsForm} from './CommentsForm'
 export {default as Comments} from './Comments'
 export {default as PostDetail} from './PostDetail'
 export { default as FeaturedPostCard } from './FeaturedPostCard';
-export {default as Loader} from './Loader'
-export { default as AdjacentPostCard } from './AdjacentPostCard';
+export {}
+
+
+
+
+// export { default as AdjacentPostCard } from './AdjacentPostCard';
 
--- .history/components/index_20220729194734.js
+++ .history/components/index_20220729194734.js
@@ -9,5 +9,9 @@ export {default as Comments} from './Comments'
 export {default as PostDetail} from './PostDetail'
 export { default as FeaturedPostCard } from './FeaturedPostCard';
 export {default as Loader} from './Loader'
-export { default as AdjacentPostCard } from './AdjacentPostCard';
+
+
+
+
+// export { default as AdjacentPostCard } from './AdjacentPostCard';
 
