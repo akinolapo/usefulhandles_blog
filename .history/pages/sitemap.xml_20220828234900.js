f75fdae17277b8bc013aa51c8e5f611e7a1ae0c6
Peace Akinola <akinolapo@gmail.com>
2022-08-28 23:49:16 +0100

    wip

---
+++ .history/pages/sitemap.xml_20220828234900.js
@@ -0,0 +1,64 @@
+
+  import React from "react";
+  import * as fs from "fs";
+  import { getPosts } from "../services";
+  const Sitemap = () => {
+    return null;
+  };
+  
+  export const getServerSideProps = async ({ res }) => {
+    const BASE_URL = "https://www.wordpressvee.com";
+  
+    const staticPaths = fs
+      .readdirSync("pages")
+      .filter((staticPage) => {
+        return ![
+          "api",
+          "product",
+          "_app.js",
+          "_document.js",
+          "404.js",
+          "sitemap.xml.js",
+        ].includes(staticPage);
+      })
+      .map((staticPagePath) => {
+        return `${BASE_URL}/${staticPagePath}`;
+      });
+  
+      const posts = await getPosts()
+      const dynamicPaths = posts.map( post => {
+    
+        return `${BASE_URL}/post/${post.node.slug}`
+        
+      })
+     
+  
+    const allPaths = [...staticPaths, ...dynamicPaths];
+  
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${allPaths
+          .map((url) => {
+            return `
+              <url>
+                <loc>${url}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+              </url>
+            `;
+          })
+          .join("")}
+      </urlset>
+    `;
+  
+    res.setHeader("Content-Type", "text/xml");
+    res.write(sitemap);
+    res.end();
+  
+    return {
+      props: {},
+    };
+  };
+  
+  export default Sitemap;
