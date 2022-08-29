2eba77a57d733c63fd3603c8d9071975175e3e18
Peace Akinola <akinolapo@gmail.com>
2022-08-29 18:45:55 +0100

    res postion

--- .history/pages/sitemap.xml_20220829184502.js
+++ .history/pages/sitemap.xml_20220829184502.js
@@ -33,10 +33,6 @@
      
   
     const allPaths = [...staticPaths, ...dynamicPaths];
-
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/xml");
-    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
   
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -55,7 +51,9 @@
       </urlset>
     `;
   
-   
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/xml");
+    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
     // res.write(sitemap);
     res.end();
   
--- .history/pages/sitemap.xml_20220829184509.js
+++ .history/pages/sitemap.xml_20220829184509.js
@@ -33,10 +33,6 @@
      
   
     const allPaths = [...staticPaths, ...dynamicPaths];
-
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/xml");
-    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
   
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
